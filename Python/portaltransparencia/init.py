44ed0b35e7f433e21990338ebbbf3efada8a4a11
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-09-14 12:36:23 -0300

    Atualizações

--- Python/portaltransparencia/init.py
+++ Python/portaltransparencia/init.py
@@ -1,7 +1,58 @@
-from graficos.despesas import despesa_por_mes_do_ano, TiposDeDespesa, despesa_acumulada_por_ano
+from graficos.despesas import (
+    despesa_por_mes_do_ano,
+    TiposDeDespesa,
+    despesa_acumulada_por_ano,
+    despesa_acumulada_todos_os_anos,
+)
 import os
 
-user_year = int(input("Digite o ano desejado: "))
-user_month = int(input("Digite o mês desejado (1 para Janeiro, 2 para Fevereiro, etc.): "))
+def selecionar_tipo_despesa():
 
-despesa_acumulada_por_ano(user_year)
+    print("Escolha o tipo de despesa:")
+    print("1. Visualizar apenas dados do tipo \"despesa\"")
+    print("2. Visualizar apenas dados do tipo \"despesaOrcamentaria\"")
+    print("3. Visualizar todos os dados")
+
+    choiceTipoDespesa = input("Escolha uma opção (1/2/3): ")
+
+    if choiceTipoDespesa == "1":
+        return TiposDeDespesa.APENAS_DESPESA
+    elif choiceTipoDespesa == "2":
+        return TiposDeDespesa.APENAS_DESPESA_ORCAMENTARIA
+    elif choiceTipoDespesa == "3":
+        return TiposDeDespesa.TODAS_AS_DESPESAS
+    else:
+        return ''
+
+def main():
+    while True:
+        print("Escolha uma opção:")
+        print("1. Consultar despesa em um mês do ano")
+        print("2. Consultar despesa acumulada de todos os meses em um ano")
+        print("3. Consultar despesa acumulada de todos os anos")
+        print("4. Sair")
+
+        choice = input("Escolha uma opção (1/2/3/4): ")
+
+        if choice == "1":
+            user_year = int(input("Digite o ano desejado: "))
+            user_month = int(input("Digite o mês desejado (1 para Janeiro, 2 para Fevereiro, etc.): "))
+            tipo_despesa = selecionar_tipo_despesa()
+
+            despesa_por_mes_do_ano(user_year, user_month, tipo_despesa)
+        elif choice == "2":
+            user_year = int(input("Digite o ano desejado: "))
+            despesa_acumulada_por_ano(user_year)
+        elif choice == "3":
+            despesa_acumulada_todos_os_anos()
+        elif choice == "4":
+            print("Saindo do programa.")
+            break
+        else:
+            print("Opção inválida!")
+            break
+
+        print()
+
+if __name__ == "__main__":
+    main()
